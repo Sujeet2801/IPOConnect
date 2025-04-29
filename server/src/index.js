4d8277b28a5e43ddf747ca97449a6b5203024c63
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-04-29 11:19:09 +0530

    feat(server): Add middleware for parsing requests and enabling CORS

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({
     path: "./.env",
@@ -7,6 +9,15 @@ dotenv.config({
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: process.env.BASE_URL,
+    })
+);
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
