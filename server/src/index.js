e874d62036fe5b5707c9afadbd2cb153e5722e9b
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-04-29 10:35:35 +0530

    feat(server): initialize express server with dotenv configuration

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env",
+});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
