eb77b889109a4b4ce9fab1df7dce3f226db44e16
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-02 02:17:43 +0000

    feat(server/routes): add login and logout routes to user router

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { registerUserController, verifyEmailController } from '../controllers/user.controller.js';
+import { loginUserController, logoutUserController, registerUserController, verifyEmailController} from '../controllers/user.controller.js';
 
 const router = Router()
 
 router.route("/register").post(registerUserController)
 router.route("/verify/:token").get(verifyEmailController)
+router.route("/login").post(loginUserController)
+router.route("/logout").post(logoutUserController)
 
 export default router;
