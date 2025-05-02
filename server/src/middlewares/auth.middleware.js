74e0162056db4e2272469bfaaaee50c1d13dfac9
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-02 12:45:49 +0000

    feat(server/middleware): Add authentication middleware to verify JWT from cookies

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+import { ApiError } from "../utils/api-error.js"
+
+export const isAuthenticated = async(req, res, next) => {
+    const token = req.cookies?.token
+
+    if(!token){
+        return res.status(401).json(new ApiError(401, "Not authenticated"))
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = { id: decoded.id }
+        next()
+    } catch (error) {
+        return res.status(401).json(new ApiError(401, "Invalid or Expired token"))
+    }
+}
