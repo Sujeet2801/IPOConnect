bf2efe9dd9b6262079c4ef2cb79936f9fd6e2580
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-03 03:11:29 +0000

    feat(server/routes): add auth middleware and user registration validation to user routes

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,11 +1,19 @@
 import { Router } from 'express'
-import { loginUserController, logoutUserController, registerUserController, verifyEmailController} from '../controllers/user.controller.js';
+import { loginUserController, logoutUserController, registerUserController, verifyEmailController, resendVerificationEmailController, forgotPasswordRequestController, changePasswordController, getCurrentUser } from '../controllers/user.controller.js';
+import { isAuthenticated } from '../middlewares/auth.middleware.js';
+import { userRegistrationValidator } from '../validate/auth.validate.js';
+import { validate } from '../middlewares/validate.middleware.js';
 
 const router = Router()
 
-router.route("/register").post(registerUserController)
+router.route("/register").post( userRegistrationValidator(), validate, registerUserController)
 router.route("/verify/:token").get(verifyEmailController)
 router.route("/login").post(loginUserController)
-router.route("/logout").post(logoutUserController)
+router.route("/logout").get( isAuthenticated, logoutUserController)
+router.route("/me").get( isAuthenticated, getCurrentUser)
+
+router.route("/resendverifyemail").post(resendVerificationEmailController)
+router.route("/passwordresetrequest").post(forgotPasswordRequestController)
+router.route("/reset-password/:token").post(changePasswordController)
 
 export default router;
