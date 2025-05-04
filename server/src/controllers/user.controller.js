b177da610a736f27988559813935a566e06bc5aa
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-04 04:55:52 +0000

    feat(server/controller): Include user role in JWT payload during login

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import bcrypt from 'bcryptjs'
 import crypto from 'crypto'
 import { UserRole } from '../generated/prisma/index.js'
 import jwt from 'jsonwebtoken'
-import { resendVerificationEmail, sendVerificationEmail } from '../utils/mail.js'
+import { resendVerificationEmail, sendForgotPasswordEmail, sendVerificationEmail } from '../utils/mail.js'
 import { ApiResponse } from '../utils/api-response.js'
 
 const registerUserController = asyncHandler( async (req, res) => {
@@ -108,7 +108,7 @@ const loginUserController = asyncHandler( async (req, res) => {
 
     // 5. Generate JWT token
     const token = jwt.sign(
-        { id: existingUser.id },
+        { id: existingUser.id, role: existingUser.role },
         process.env.JWT_SECRET,
         { expiresIn: "7d" }
     );
@@ -132,9 +132,126 @@ const logoutUserController = asyncHandler( async (req, res) => {
     res.status(200).json(new ApiResponse(200, {}, "Logout successful"));
 });
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+    
+    const existingUser = await db.user.findUnique({
+        where: { id: req.user.id }
+    })
+
+    if (!existingUser) {
+        return res.status(404).json(new ApiError(404, "User not found"));
+    }
+
+    res.status(200).json(new ApiResponse(200, existingUser, "User fetched successfully"));
+})
+
+const resendVerificationEmailController = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json(new ApiError(400, "Email is required"));
+    }
+
+    const existingUser = await db.user.findUnique({ where: { email } });
+
+    if (!existingUser) {
+        return res.status(404).json(new ApiError(404, "User does not exist"));
+    }
+
+    const verificationToken = crypto.randomBytes(32).toString("hex");
+
+    await db.user.update({
+        where: { id: existingUser.id },
+        data: {
+            emailVerificationToken: verificationToken,
+            emailVerificationExpiry: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
+        },
+    });
+
+    await resendVerificationEmail({
+        email,
+        name: existingUser.name,
+        verificationToken,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { existingUser, verificationToken }, "Verification email resent successfully."));
+});
+
+const forgotPasswordRequestController = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    if(!email){
+        return res.status(400).json(new ApiError(400, "Email is required"))
+    }
+
+    const existingUser = await db.user.findUnique({ where: { email }})
+    if(!existingUser){
+        return res.status(404).json(new ApiError(404, "User not exists"))
+    }
+
+    const resetToken = crypto.randomBytes(32).toString("hex")
+
+    await db.user.update({
+        where: { email },
+        data: {
+            forgotPasswordToken: resetToken,
+            forgotPasswordExpiry: new Date(Date.now() + 60 * 60 * 1000)
+        }
+    })
+
+    await sendForgotPasswordEmail({
+        email,
+        name: existingUser.name,
+        resetToken: resetToken
+    })
+
+    return res.status(201).json(new ApiResponse(201, {}, "Email sent successfully to reset password"))
+})
+
+const changePasswordController = asyncHandler(async (req, res) => {
+
+    const { token } = req.params;
+    const { newPassword } = req.body;
+
+    if (!newPassword) {
+        return res.status(400).json(new ApiError(400, "Password cannot be empty"));
+    }
+
+    const existingUser = await db.user.findFirst({
+        where: {
+            forgotPasswordToken: token,
+            forgotPasswordExpiry: {
+                gte: new Date(),
+            },
+        },
+    });
+
+    if (!existingUser) {
+        return res.status(404).json(new ApiError(404, "Invalid or expired token"));
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    await db.user.update({
+        where: { id: existingUser.id },
+        data: {
+            password: hashedPassword,
+            forgotPasswordToken: null,
+            forgotPasswordExpiry: null,
+        },
+    });
+
+    return res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
 export { 
     registerUserController,
     verifyEmailController,
     loginUserController,
     logoutUserController,
+    getCurrentUser,
+    resendVerificationEmailController,
+    forgotPasswordRequestController,
+    changePasswordController
 };
