ddc82ffd6cb4d4859cd8475c277cf752c889ee9f
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-04 05:02:35 +0000

    feat(server/routes): Implement authentication and role-based access for IPO routes

--- server/src/routes/ipo.route.js
+++ server/src/routes/ipo.route.js
@@ -1,12 +1,18 @@
 import { Router } from 'express'
 import { deleteIpoController, getAllIpoController, getIposByStatusController, registerIpoController, updateIpoController } from '../controllers/ipo.controller.js'
+import { isAuthenticated } from '../middlewares/auth.middleware.js'
+import { authorizeRoles } from '../middlewares/authRole.middleware.js'
 
 const router = Router()
 
-router.route("/ipo/register").post(registerIpoController)
-router.route("/ipo/:companyName").put(updateIpoController)
+router.route("/ipo/register").post( isAuthenticated, authorizeRoles("ADMIN"), registerIpoController)
+
+router.route("/ipo/:companyName").put(isAuthenticated, authorizeRoles("ADMIN"),updateIpoController)
+
+router.route("/ipo/:companyName").delete(isAuthenticated, authorizeRoles("ADMIN"),deleteIpoController)
+
 router.route("/ipos").get(getAllIpoController)
+
 router.route("/ipo/by-status").get(getIposByStatusController)
-router.route("/ipo/:companyName").delete(deleteIpoController)
 
 export default router
