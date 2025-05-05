b3620d9546ce3d439f918524803340d0993f84dc
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-05 04:17:46 +0000

    bugfix(server/route/user): Add login validator

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,14 +1,14 @@
 import { Router } from 'express'
 import { loginUserController, logoutUserController, registerUserController, verifyEmailController, resendVerificationEmailController, forgotPasswordRequestController, changePasswordController, getCurrentUser } from '../controllers/user.controller.js';
 import { isAuthenticated } from '../middlewares/auth.middleware.js';
-import { userRegistrationValidator } from '../validate/auth.validate.js';
+import { userLoginValidator, userRegistrationValidator } from '../validate/auth.validate.js';
 import { validate } from '../middlewares/validate.middleware.js';
 
 const router = Router()
 
 router.route("/register").post( userRegistrationValidator(), validate, registerUserController)
 router.route("/verify/:token").get(verifyEmailController)
-router.route("/login").post(loginUserController)
+router.route("/login").post( userLoginValidator(), validate, loginUserController)
 router.route("/logout").get( isAuthenticated, logoutUserController)
 router.route("/me").get( isAuthenticated, getCurrentUser)
 
