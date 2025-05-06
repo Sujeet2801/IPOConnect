1b2574df2c57275a3a9e558aae5d609e8cc46098
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-06 02:46:49 +0000

    feat(server/routes): Protect sector routes with auth, role-based access, and validation middleware

---
+++ server/src/routes/sector.route.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { createSectorController, updateSectorController, getAllSectorController,
+getSectorsByMarketCapTypeController, deleteSectorController } from "../controllers/sector.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middlewares/authRole.middleware.js";
+import { sectorValidationRules } from "../validate/sector.validate.js";
+import { validate } from "../middlewares/validate.middleware.js";
+
+const router = Router();
+
+router.route("/sector")
+    .post(isAuthenticated, authorizeRoles("ADMIN"), sectorValidationRules(), validate, createSectorController);
+
+router.route("/sector/:sectorNameFromParams").put(isAuthenticated, authorizeRoles("ADMIN"), updateSectorController);
+
+router.route("/sector/:sectorName").delete(isAuthenticated, authorizeRoles("ADMIN"), deleteSectorController);
+
+router.route("/sectors").get(getAllSectorController);
+
+router.route("/sectors/marketcaps/:type").get(getSectorsByMarketCapTypeController);
+
+export default router;
