1f77540359271a2f4aae832df8f7a93588d7c11b
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-07 04:35:19 +0000

    feat(server/validators): Add request validation to user auth routes using express-validator middleware

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,16 +1,16 @@
 import { Router } from 'express'
 import { loginUserController, logoutUserController, registerUserController, verifyEmailController, resendVerificationEmailController, forgotPasswordRequestController, changePasswordController, getCurrentUser } from '../controllers/user.controller.js';
 import { isAuthenticated } from '../middlewares/auth.middleware.js';
-import { userLoginValidator, userRegistrationValidator } from '../validate/auth.validate.js';
+import { userLoginValidator, userRegistrationValidator } from '../validators/auth.validate.js';
 import { validate } from '../middlewares/validate.middleware.js';
 
 const router = Router()
 
-router.route("/register").post( userRegistrationValidator(), validate, registerUserController)
+router.route("/register").post(userRegistrationValidator(), validate, registerUserController)
 router.route("/verify/:token").get(verifyEmailController)
-router.route("/login").post( userLoginValidator(), validate, loginUserController)
-router.route("/logout").get( isAuthenticated, logoutUserController)
-router.route("/me").get( isAuthenticated, getCurrentUser)
+router.route("/login").post(userLoginValidator(), validate, loginUserController)
+router.route("/logout").get(isAuthenticated, logoutUserController)
+router.route("/me").get(isAuthenticated, getCurrentUser)
 
 router.route("/resendverifyemail").post(resendVerificationEmailController)
 router.route("/passwordresetrequest").post(forgotPasswordRequestController)
