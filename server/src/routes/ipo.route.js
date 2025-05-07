e7edcd88d7439ca4efb2907b3c9482e15c01c8f7
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-07 04:39:26 +0000

    feat(server/routes): Protect IPO routes with authentication and role-based authorization middleware

--- server/src/routes/ipo.route.js
+++ server/src/routes/ipo.route.js
@@ -1,15 +1,25 @@
 import { Router } from 'express'
-import { deleteIpoController, getAllIpoController, getIposByStatusController, registerIpoController, updateIpoController } from '../controllers/ipo.controller.js'
+import {
+    deleteIpoController,
+    getAllIpoController,
+    getIposByStatusController,
+    registerIpoController,
+    updateIpoController
+}
+    from '../controllers/ipo.controller.js'
 import { isAuthenticated } from '../middlewares/auth.middleware.js'
 import { authorizeRoles } from '../middlewares/authRole.middleware.js'
+import { ipoValidationRules } from '../validators/ipo.validate.js'
+import { validate } from '../middlewares/validate.middleware.js'
 
 const router = Router()
+1
+router.route("/ipo/register")
+    .post(isAuthenticated, authorizeRoles("ADMIN"), ipoValidationRules(), validate, registerIpoController)
 
-router.route("/ipo/register").post( isAuthenticated, authorizeRoles("ADMIN"), registerIpoController)
+router.route("/ipo/:companyName").put(isAuthenticated, authorizeRoles("ADMIN"), updateIpoController)
 
-router.route("/ipo/:companyName").put(isAuthenticated, authorizeRoles("ADMIN"),updateIpoController)
-
-router.route("/ipo/:companyName").delete(isAuthenticated, authorizeRoles("ADMIN"),deleteIpoController)
+router.route("/ipo/:companyName").delete(isAuthenticated, authorizeRoles("ADMIN"), deleteIpoController)
 
 router.route("/ipos").get(getAllIpoController)
 
