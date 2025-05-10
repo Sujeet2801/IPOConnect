fb6b8fef25081da85a0109161a16dab969f6ca60
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-10 22:02:43 +0530

    Tailwind and required package installed (#2)
    * chore(admin): Update document title to "IPOConnect admin
    
    * chore(admin): Tailwind and package installed

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+  proxy: {
+    "/api/v1/users": {
+      target: "https://ipoconnect-production.up.railway.app",
+      changeOrigin: true,
+      secure: false,
+    }
+  }
+
+  },
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
 })
