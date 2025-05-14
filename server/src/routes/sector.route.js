c97e8f3fbd116d471660476f44f4b383168a8cfa
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-14 11:13:44 +0530

    feat(server/routes): Add protected GET /sectors/:sectorName route to fetch sector details by name

--- server/src/routes/sector.route.js
+++ server/src/routes/sector.route.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { createSectorController, updateSectorController, getAllSectorController,
-getSectorsByMarketCapTypeController, deleteSectorController } from "../controllers/sector.controller.js";
+import { createSectorController, 
+    updateSectorController, 
+    getAllSectorController,
+    getSectorsByMarketCapTypeController, 
+    deleteSectorController, 
+    getSectorDeatail } 
+    from "../controllers/sector.controller.js";
+
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../middlewares/authRole.middleware.js";
 import { sectorValidationRules } from "../validators/sector.validate.js";
@@ -8,15 +14,28 @@ import { validate } from "../middlewares/validate.middleware.js";
 
 const router = Router();
 
-router.route("/sector")
+router
+    .route("/sector")
     .post(isAuthenticated, authorizeRoles("ADMIN"), sectorValidationRules(), validate, createSectorController);
 
-router.route("/sector/:sectorNameFromParams").put(isAuthenticated, authorizeRoles("ADMIN"), updateSectorController);
+router
+    .route("/sector/:sectorNameFromParams")
+    .put(isAuthenticated, authorizeRoles("ADMIN"), updateSectorController);
 
-router.route("/sector/:sectorName").delete(isAuthenticated, authorizeRoles("ADMIN"), deleteSectorController);
+router
+    .route("/sector/:sectorName")
+    .delete(isAuthenticated, authorizeRoles("ADMIN"), deleteSectorController);
 
-router.route("/sectors").get(getAllSectorController);
+router
+    .route("/sectors")
+    .get(getAllSectorController);
 
-router.route("/sectors/marketcaps/:type").get(getSectorsByMarketCapTypeController);
+router
+    .route("/sectors/:sectorName")
+    .get( isAuthenticated, getSectorDeatail)
+
+router
+    .route("/sectors/marketcaps/:type")
+    .get( isAuthenticated, getSectorsByMarketCapTypeController);
 
 export default router;
