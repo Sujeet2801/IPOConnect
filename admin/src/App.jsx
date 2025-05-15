0060c2cded4c1421f5a4c2a74382534d4c94be55
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-15 09:18:04 +0530

    feat(admin): Add Layout wrapper and define /login and /register routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import { AuthProvider } from "./hooks/useAuth";
 
 function App() {
-
   return (
-    <>
-      Hello from ipoconnect admin
-    </>
-  )
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          </Route>
+
+          <Route path="/login" element={<Layout />} />
+          <Route path="/register" element={<Layout />} />
+
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
