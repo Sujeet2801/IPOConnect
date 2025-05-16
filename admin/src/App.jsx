b54691e05590db04a499e7b87c9ed14ea7e623d5
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-16 16:46:11 +0530

    feat(admin): Add blog routes for listing, creating, and editing blogs in App file

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,12 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import { AuthProvider } from "./hooks/useAuth";
 
+import IposPage from "./pages/Ipo/IposPage";
+
+import Blog from "./pages/Blog/Blog";
+import UpdateBlog from "./pages/Blog/UpdateBlog";
+import AddBlog from "./pages/Blog/AddBlog";
+
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Layout />}>
+            <Route path="/ipos" element={<IposPage />} />
+            <Route index element={<IposPage />} />
+
+            <Route path="/blogs" element={<Blog />} />
+            <Route path="blog/edit/:titleSlug" element={<UpdateBlog />} />
+            <Route path="/blog/create" element={<AddBlog />} />
           </Route>
 
           <Route path="/login" element={<Layout />} />
