de0ae123e5a83bb2faf3e33b7f587e7b0e754b0c
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-24 08:23:20 +0530

    feat(admin): Add routes for IPO listing, addition, and editing in App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,8 @@ import Layout from "./components/Layout/Layout";
 import { AuthProvider } from "./hooks/useAuth";
 
 import IposPage from "./pages/Ipo/IposPage";
+import AddIpoPage from "./pages/Ipo/AddIpoPage";
+import EditIpoPage from "./pages/Ipo/EditIpoPage";
 
 import Blog from "./pages/Blog/Blog";
 import UpdateBlog from "./pages/Blog/UpdateBlog";
@@ -14,8 +16,10 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="/ipos" element={<IposPage />} />
             <Route index element={<IposPage />} />
+            <Route path="/ipos" element={<IposPage />} />
+            <Route path="/ipo/add" element={<AddIpoPage />} />
+            <Route path="/ipo/edit/:companySlug" element={<EditIpoPage />} />
 
             <Route path="/blogs" element={<Blog />} />
             <Route path="blog/edit/:titleSlug" element={<UpdateBlog />} />
@@ -24,7 +28,6 @@ function App() {
 
           <Route path="/login" element={<Layout />} />
           <Route path="/register" element={<Layout />} />
-
         </Routes>
       </AuthProvider>
     </BrowserRouter>
